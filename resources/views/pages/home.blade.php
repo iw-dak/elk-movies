801f9a1dbcbdd35b6e74e235e164d2c3d75d7400
Adam Malick sow <dachic@Adams-MacBook-Pro.local>
2019-07-21 21:12:42 +0200

    home css fixed

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,11 +1,7 @@
 @extends('template')
 
 @section('content')
-
-    <div class="overlay"></div>
-      <div class="container-large">
-        @include('pages._partials.main-movies', ['movies' => $movies])
-      </div>
-    <div id="home"></div>
-		
+  <div class="container-large">
+    @include('pages._partials.main-movies', ['movies' => $movies])
+  </div>
 @endsection
